55c024ef5061009dbde2ddab54e1511cf8758255
gengjiaxiang <gegnjiaxiang@alibaba.com>
2019-06-27 16:34:07 +0800

    1. 项目基本结构搭建完成 2. 登录功能实现了一半 3. 我要开始招聘程序员了

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-Vue.config.productionTip = false
+// 1. 引入EelementUI库
+import ElementUI from "element-ui";
+// 2. 引入CSS文件
+import "element-ui/lib/theme-chalk/index.css";
+
+import "./assets/css/index.css";
+
+// 3. 安装ElementUI
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
