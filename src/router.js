30b57c3d3229dcf495ca6431d6775e7d61c33470
gengjiaxiang <gegnjiaxiang@alibaba.com>
2019-06-29 16:50:04 +0800

    侧边导航菜单实现

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Login from "./views/Login.vue";
 import Home from "./views/Home.vue";
+import User from './views/User.vue';
 
 Vue.use(Router);
 
@@ -13,7 +14,15 @@ const router = new Router({
     },
     {
       path: "/home",
-      component: Home
+      component: Home,
+      children: [
+        {
+          // 如果给path加上/  变成/user   那么访问的时候直接 使用 /user
+          // 如果不加/  那么访问的时候直接  /home/user
+          path: "/user",
+          component: User
+        }
+      ]
     },
     {
       path: "/",
---
+++ src/views/User.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>这是用户列表页</h1>
+  </div>
+</template>
